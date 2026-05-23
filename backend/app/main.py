e343e3ac993eceef202375066f50c9f8dd87131c
Daniel Sobowale <danbowale@gmail.com>
2026-05-22 23:31:52 -0500

    added vercel endpoint to fastapi cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app.include_router(main_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5173"],
+    allow_origins = ["http://localhost:5173", "https://clippper.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
